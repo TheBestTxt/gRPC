cbd31bfbcae5d900da5eba3ed9314892c8b6048d
txt <492596975@qq.com>
2021-05-26 21:07:42 +0800

    添加项目文件。

---
+++ TheBestTxt.SocketAbout.RPC.gRPCClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using TheBestTxt.SocketAbout.RPC.gRPCService;
+using Grpc.Net.Client;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Grpc.Core;
+
+namespace TheBestTxt.SocketAbout.RPC.gRPCClient
+{
+    class Program
+    {
+        private static List<string> _attackMethod = new List<string>()
+        {
+            "飞踢",
+            "重拳",
+            "精神"
+        };
+
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "Txt" });
+            Console.WriteLine("调用Greeter服务 : " + reply.Message);
+
+            var fight = new Fight.FightClient(channel);
+            var fightReply = await fight.AttackAsync(new Google.Protobuf.WellKnownTypes.Empty());
+            Console.WriteLine("调用Fight服务 : " + fightReply.Message);
+
+            var fightTogether = fight.AttackTogetoher();
+            for (int i = 0; i < 10; i++)
+            {
+                Random random = new Random();
+                await fightTogether.RequestStream.WriteAsync(new AttackTogetoherRequest { Method = _attackMethod[random.Next(0, 3)] });
+            }
+
+            var fightResponseRead = Task.Run(async () =>
+            {
+                try
+                {
+                    await foreach (var resp in fightTogether.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine($"受到了来自Fight服务端的{resp.Method}攻击");
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                {
+                    Console.WriteLine("Stream cancelled.");
+                }
+            });
+
+            await fightTogether.RequestStream.CompleteAsync();
+            Console.WriteLine("发起攻击完毕");
+            await fightResponseRead;
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TheBestTxt.SocketAbout.RPC.gRPCService/Services/FightService.cs
@@ -0,0 +1,56 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBestTxt.SocketAbout.RPC.gRPCService
+{
+    public class FightService : Fight.FightBase
+    {
+        private readonly List<string> _attackMethod = new List<string>()
+        {
+            "飞踢",
+            "重拳",
+            "精神"
+        };
+
+        private readonly ILogger<FightService> _logger;
+
+        public FightService(ILogger<FightService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AttackResponse> Attack(Empty request, ServerCallContext context)
+        {
+            Random random = new Random();
+            return Task.FromResult(new AttackResponse()
+            {
+                Message = $"发起了{_attackMethod[random.Next(0, 3)]}攻击"
+            });
+        }
+
+        public async override Task AttackTogetoher(IAsyncStreamReader<AttackTogetoherRequest> requestStream, IServerStreamWriter<AttackTogetoherResponse> responseStream, ServerCallContext context)
+        {
+            var list = new List<AttackTogetoherRequest>();
+            int i = 1;
+            while (await requestStream.MoveNext())
+            {
+                list.Add(requestStream.Current);
+                _logger.LogInformation($"添加第{i.ToString()}次操作完成");
+                i++;
+            }
+            for (int j = 0; j < list.Count; j++)
+            {
+                _logger.LogInformation($"受到来自客户端的{list[j].Method}攻击");
+                Random random = new Random();
+                await responseStream.WriteAsync(new AttackTogetoherResponse() { Method = _attackMethod[random.Next(0, 3)] });
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
